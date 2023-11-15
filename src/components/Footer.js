790510f51ea5bfb3b3b4360e597475d84b89000e
Wiibleyde <nathan@bonnell.fr>
2023-11-15 16:28:40 +0100

    Refactor footer and live components
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ import {
 function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 w-full bg-green-700 text-white p-4 flex flex-row justify-between items-center" id="footer">
-            <div className="links flex flex-row justify-between">
-                <a href="https://github.com/Wiibleyde/TimeAttack" className="button mx-2 bg-green-600"><Github className="" />Github</a>
+            <div className="">
+                <a href="https://github.com/Wiibleyde/TimeAttack" className="button"><Github className="" />Github</a>
             </div>
         </footer>
     );
--- src/components/Live.js
+++ src/components/Live.js
@@ -1,45 +1,68 @@
 import React from 'react';
 import axios from 'axios';
+import useInterval from '../useInterval';
+
+async function fetchData() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await axios.get('http://localhost:3001/api/raceLeaderboard?raceId=1', {
+                headers: {
+                    'Authorization': 'FIrtp3TpCLW8e1Mg5oSo7crcB1uYKvI7xgg257UDBiRsKgqsw5hEVBOSl9OaGTXZ',
+                },
+                withCredentials: true
+            })
+            resolve(response.data);
+        } catch (error) {
+            reject(error);
+        }
+    })    
+}
 
 function Live() {
-    let leaderboard = [];
-    const url = "https://api-ta.bonnell.fr/api/raceLeaderboard?raceId=1";
-    // Axios get with Authorization header
-    axios.get(url, {
-        headers: {
-            'Authorization': 'WsSQR7xj7LKmBYmOf7gXDDPstfcXQkvy1le18PRHEhULdq84w5YgTZClSEKMpdSP',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
-        },
-        withCredentials: true
-    })
-    .then((response) => {
-        console.log(response);
-        leaderboard = response.data;
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-    console.log(leaderboard);
+    const [leaderboard, setLeaderboard] = React.useState([]);
+    React.useEffect(() => {
+        fetchData().then((data) => {
+            setLeaderboard(data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, []);
 
+    React.useEffect(() => {
+        const header = document.getElementById('header');
+        const footer = document.getElementById('footer');
+        header.classList.add('hidden');
+        footer.classList.add('hidden');
+        return () => {
+            header.classList.remove('hidden');
+            footer.classList.remove('hidden');
+        }
+    });
+
+    useInterval(() => {
+        fetchData().then((data) => {
+            setLeaderboard(data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+    , 500);
+    
     return (
         <div className="Live">
-            {/* Leaderboard from time attack API */}
             <div className="leaderboard">
-                <h1 className="title text-3xl">Leaderboard</h1>
                 <div className="leaderboardTable">
-                    <table>
-                        <thead>
-                            <tr>
+                    <table className="table-auto w-full text-center">
+                        <thead className="bg-green-600 text-white">
+                            <tr className="border-b-2 border-green-600">
                                 <th>Position</th>
                                 <th>Nom</th>
                                 <th>Temps</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody className="text-white">
                             {leaderboard.map((racer, index) => (
-                                <tr key={index}>
+                                <tr key={index} className="border-b-2 border-green-600 {index == 1 ? 'bg-green-700' : 'bg-green-600'}">
                                     <td>{index + 1}</td>
                                     <td>{racer.RacerName}</td>
                                     <td>{racer.Duration}</td>
