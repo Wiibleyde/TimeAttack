c5af75d7179213b036450a040ff74cc714496c5d
Wiibleyde <nathan@bonnell.fr>
2023-11-29 09:21:01 +0100

    Update API endpoint in Live.js
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/components/Live.js
+++ src/components/Live.js
@@ -5,11 +5,12 @@ import useInterval from '../useInterval';
 async function fetchData() {
     return new Promise(async (resolve, reject) => {
         try {
-            const response = await axios.get('http://localhost:3001/api/raceLeaderboard?raceId=1', {
+            const response = await axios.get('https://api-ta.bonnell.fr/api/raceLeaderboard?raceId=1', {
                 headers: {
-                    'Authorization': 'FIrtp3TpCLW8e1Mg5oSo7crcB1uYKvI7xgg257UDBiRsKgqsw5hEVBOSl9OaGTXZ',
-                },
-                withCredentials: true
+                    'Authorization': 'WsSQR7xj7LKmBYmOf7gXDDPstfcXQkvy1le18PRHEhULdq84w5YgTZClSEKMpdSP',
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
             })
             resolve(response.data);
         } catch (error) {
@@ -47,7 +48,33 @@ function Live() {
         })
     }
     , 500);
-    
+
+    if (leaderboard === "Unauthorized") {
+        return (
+            <div className="Live">
+                <div className="leaderboard">
+                    <div className="leaderboardTable">
+                        <table className="table-auto w-full text-center">
+                            <thead className="bg-green-600 text-white">
+                                <tr className="border-b-2 border-green-600">
+                                    <th>Position</th>
+                                    <th>Nom</th>
+                                    <th>Temps</th>
+                                </tr>
+                            </thead>
+                            <tbody className="text-white">
+                                <tr className="border-b-2 border-green-600 bg-green-600">
+                                    <td>Chargement</td>
+                                    <td>Chargement</td>
+                                    <td>Chargement</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="Live">
             <div className="leaderboard">
@@ -62,7 +89,7 @@ function Live() {
                         </thead>
                         <tbody className="text-white">
                             {leaderboard.map((racer, index) => (
-                                <tr key={index} className="border-b-2 border-green-600 {index == 1 ? 'bg-green-700' : 'bg-green-600'}">
+                                <tr key={index} className="border-b-2 border-green-600">
                                     <td>{index + 1}</td>
                                     <td>{racer.RacerName}</td>
                                     <td>{racer.Duration}</td>
