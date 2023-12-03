364d42aa7f187d555bf77dea8875b9bcf9620126
Wiibleyde <nathan@bonnell.fr>
2023-12-03 22:28:12 +0100

    Update link in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
             <div className="App bg-green-700 text-white rounded-lg p-4 m-4 flex flex-col justify-center items-center">
             <h1 className="title text-3xl font-bold text-center">Bienvenue sur le site de gestion de course de Cali</h1>
             <div className="buttons links flex flex-row justify-between">
-                <NavLink to="/racers" className="button mx-2 bg-green-600 py-1 px-2 rounded-lg">Liste des coureurs</NavLink>
+                <NavLink to="/live" className="button mx-2 bg-green-600 py-1 px-2 rounded-lg">Meilleurs temps en live</NavLink>
                 <NavLink to="/edit" className="button mx-2 bg-green-600 py-1 px-2 rounded-lg">Ajouter un coureur</NavLink>
             </div>
         </div>
